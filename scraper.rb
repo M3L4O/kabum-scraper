3de4cf0cc1cada437b6ff219c02be124d341c388
M3L4O <joaovictor.slv@ufpi.edu.br>
2023-09-05 13:17:41 -0300

    feat: add main app

---
+++ scraper.rb
@@ -0,0 +1,30 @@
+require 'nokogiri'
+require 'open-uri'
+
+require_relative 'app/model/product'
+require_relative 'repository/product_repository'
+
+base_url = 'https://www.kabum.com.br'
+processors_url = '/hardware/processadores?page_number=%d&page_size=100'
+
+initial_page = base_url + (processors_url % 1)
+doc = Nokogiri::HTML(URI.open(initial_page))
+last_page = doc.css('.page')[-1].content.to_i
+i = 1
+
+repository = ProductRepository.new
+
+while i <= last_page
+  if i != 1
+    page = base_url + (processors_url % i)
+    doc = Nokogiri::HTML(URI.open(page))
+  end
+  (doc.css('.nameCard').zip(doc.css('.priceCard'), doc.css('.productLink')).map do |name, price, link|
+    product = Product.new(name.content, price.text, base_url + link['href'])
+    repository.add(product)
+  end)
+  i += 1
+end
+
+repository.select_all
+repository.select_by_name('AMD Ryzen 7 5700G')
